597c892dbabb1b69a6cf9630678346c654bf7089
Cesar Roux Dit Buisson <cr1013@imperial.ac.uk>
2015-09-15 10:49:38 +0100

    Remove uneeded abstract type dependency test

--- generator/tests/abstractdependency.ml
+++
@@ -1,5 +0,0 @@
-open Abstracttype
-
-let test = create_container 10
-
-let f = get_value test
--- generator/tests/abstracttype.mli
+++
@@ -1,4 +0,0 @@
-type container
-val empty : container
-val create_container : int -> container
-val get_value : container -> int
