4a8e3ef9b2b22be86d8ed1aef2bd4726cd017011
Thomas Wood <thomas.wood09@imperial.ac.uk>
2015-09-15 14:22:45 +0100

    Manually patch LibList.ml for partial application/function syntax.

--- generator/tests/lambda/LibList.ml
+++ generator/tests/lambda/LibList.ml
@@ -1,8 +1,6 @@
 (** val fold_right : ('a1 -> 'a2 -> 'a2) -> 'a2 -> 'a1 list -> 'a2 **)
 
-let fold_right f =
-  let rec fold_right0 f0 acc = function
+let rec fold_right f acc l = match l with
   | [] -> acc
-  | x :: l' -> f0 x (fold_right0 f0 acc l')
-  in fold_right0 f
+  | x :: l' -> f x (fold_right f acc l')
 
